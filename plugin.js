7ccd20bc93b71b67e2eac405aeca20dbbe33e6c1
yardameus <yardameus@yahoo.com>
2016-07-02 10:20:27 -0500

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -16,9 +16,9 @@ define(['playbackManager', 'pluginManager', 'browser', 'connectionManager', 'eve
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Flow';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'flow';
 
         var dependencyPrefix = self.id;
         var settingsObjectName = dependencyPrefix + '/skinsettings';
